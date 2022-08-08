115974f429ed3ba6f9b509ad99ba83e3dc5636de
Thomas <malifarge.thomas9@gmail.com>
2022-08-08 16:30:35 +0200

    clean

--- routes/id.js
+++ routes/id.js
@@ -1,45 +1,25 @@
 const express = require('express')
 const app = express()
-
-const authors = [
-    {
-        id: 1,
-        name: "Lawrence Nowell",
-        nationality: "UK",
-        books: ["Beowulf"]
-    },
-    {
-        id: 2,
-        name: "William Shakespeare",
-        nationality: "UK",
-        books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
-    },
-    {
-        id: 3,
-        name: "Charles Dickens",
-        nationality: "US",
-        books: ["Oliver Twist", "A Christmas Carol"]
-    },
-    {
-        id: 4,
-        name: "Oscar Wilde",
-        nationality: "UK",
-        books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
-    },
-]
-
-
+const authors = require('../author.json')
 
 app.get('/:id', (req, res) =>{
-    let authorsID = req.params.id;
-    res.send(`${authors[authorsID-1].name}, ${authors[authorsID-1].nationality}`
-    )
+    const {id} = req.params
+    const author = authors.find(author => author.id ===Number (id))
+    if (author){
+        res.send(`${author.name}, ${author.nationality}`)
+    }else{
+        res.status(404).send("Not Found")
+    }
 })
 
 app.get('/:id/books', (req, res)=>{
-    let authorsID = req.params.id;
-    res.send(`${authors[authorsID-1].books
-    }`)
+    const {id} = req.params
+    const author = authors.find(author => author.id ===Number (id))
+    if (author){
+        res.send(`${author.books}`)
+    }else {
+        res.status(404).send("Not Found")
+    }
 })
 
 module.exports = app
--- routes/json.js
+++ routes/json.js
@@ -1,49 +1,32 @@
 
 const express = require('express')
 const app = express()
-
-const authors = [
-    {
-        id: 1,
-        name: "Lawrence Nowell",
-        nationality: "UK",
-        books: ["Beowulf"]
-    },
-    {
-        id: 2,
-        name: "William Shakespeare",
-        nationality: "UK",
-        books: ["Hamlet", "Othello", "Romeo and Juliet", "MacBeth"]
-    },
-    {
-        id: 3,
-        name: "Charles Dickens",
-        nationality: "US",
-        books: ["Oliver Twist", "A Christmas Carol"]
-    },
-    {
-        id: 4,
-        name: "Oscar Wilde",
-        nationality: "UK",
-        books: ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
-    },
-]
-
+const authors = require('../author.json')
 
 
 app.get('/:id', (req, res) =>{
-    let authorsID = req.params.id;
-    res.json({
-        name : authors[authorsID-1].name, 
-        nationality : authors[authorsID-1].nationality}
-    )
+    const {id} = req.params
+    const author = authors.find(author => author.id ===Number (id))
+    if (author){
+        res.json({
+            name : author.name, 
+            nationality : author.nationality}
+        )
+    }else{
+        res.status(404).send("Not Found")
+    }
 })
 
 app.get('/:id/books', (req, res)=>{
-    let authorsID = req.params.id;
-    res.json({
-        books : authors[authorsID-1].books
-    })
+    const {id} = req.params
+    const author = authors.find(author => author.id ===Number (id))
+    if (author){
+        res.json({
+            books : author.books
+        })
+    } else{
+        res.status(404).send("Not Found")
+    }
 })
 
 module.exports = app
